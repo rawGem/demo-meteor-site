c41f41a1e427ea9c94c6b3d8b9156b7bbdb0fc01
rawGem <sj.browne@gatech.edu>
2015-07-05 05:09:53 -0400

    sideNav closes on click

--- client/templates/container.js
+++ client/templates/container.js
@@ -2,7 +2,9 @@
 SBJCTX = new ReactiveVar();
 
 Template.side.onRendered( function() {
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav(
+    {closeOnClick: true}
+  );
 });
 
 Template.card.helpers({
@@ -13,7 +15,8 @@ Template.card.helpers({
 
 Template.side.events({
   'click a': function(e,t) {
-    FlowRouter.go('/section/'+e.target.innerText.toLowerCase())
+    e.preventDefault();
+    FlowRouter.go('/section/'+e.target.innerText.toLowerCase());
     //SBJCTX.set(e.target.innerText);
   }
 })
