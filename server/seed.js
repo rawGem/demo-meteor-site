bf5fad8b9ecd219c38c1596f86ad33064a4c331b
rawGem <sj.browne@gatech.edu>
2015-07-04 23:52:53 -0400

    adds a section in seeds

--- server/seed.js
+++ server/seed.js
@@ -4,13 +4,22 @@ Meteor.startup(function(){
   if (Sections.find().fetch().length === 0) {
     console.log("seeding db...")
 
-    Sections.insert({
-      subject: "Education", 
-      subsections : [
-        { heading : "cs101", text_region: "i lernt da codez"},
-        { heading : "cs102", text_region: "lernt moar cdez"}
-      ]
-    });
+    Sections.insert( 
+      {
+        subject: "Education", 
+        subsections : [
+          { heading : "School 1", text_region: "i lernt da codez"},
+          { heading : "School 2", text_region: "lernt moar cdez"}
+        ]
+      });
 
+    Sections.insert(
+      {
+        subject: "Coursework", 
+        subsections : [
+          { heading : "cs101", text_region: "i lernt da codez"},
+          { heading : "cs102", text_region: "lernt moar cdez"}
+        ]
+      });
   }
-})
+});
