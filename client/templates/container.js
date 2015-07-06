e993e391ea6786f37dde40b9896e9af213bd38c4
rawGem <sj.browne@gatech.edu>
2015-07-05 21:49:35 -0400

    [DEBUG ME] can only use _id as selector to update in mongo on client

--- client/templates/container.js
+++ client/templates/container.js
@@ -14,6 +14,28 @@ Template.side.events({
   }
 })
 
+Template.card.events({
+  'click i.fa.fa-thumbs-o-up' : function(e,t) {
+    e.preventDefault();
+    console.log("** thumbs up **");
+    console.log("id: ", this._id);
+    console.log("this: ", this);
+    console.log("***************");
+    console.log("parent data id: ", Template.parentData()._id);
+    console.log("instance data: ", t.data);
+    Sections.update({
+                      _id : Template.parentData()._id, 
+                      "subsections.heading" : t.data.heading
+                    },
+                      { $inc : {"subsections.$.upvotes" : 1 }}
+                   );
+  },
+  'click': function(e,t) {
+    console.log("outer this? " , this)
+  }
+  
+});
+
 
 //Template.main.helpers({
 //  sections: function() {
