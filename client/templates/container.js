b8e58b091ff8798a80ecdc0f04d784e31a4dc86c
Grapefruit <me@here.now>
2015-08-20 17:48:59 -0400

    adds centered, working links to navbar

--- client/templates/container.js
+++ client/templates/container.js
@@ -9,12 +9,21 @@ Template.side.onRendered( function() {
 });
 
 Template.side.events({
-  'click li#edu-li': function(e,t) {
+  'click li#edu-side': function(e,t) {
     e.preventDefault();
     //FlowRouter.go('/section/'+e.target.innerText.toLowerCase().trim());
     FlowRouter.go('/section/education');
   },
-  "click li#crswrk-li": function(e,t) {
+  'click li#crswrk-side': function(e,t) {
+    e.preventDefault();
+    FlowRouter.go('/section/coursework');
+  },
+  'click li#edu-top': function(e,t) {
+    e.preventDefault();
+    //FlowRouter.go('/section/'+e.target.innerText.toLowerCase().trim());
+    FlowRouter.go('/section/education');
+  },
+  'click li#crswrk-top': function(e,t) {
     e.preventDefault();
     FlowRouter.go('/section/coursework');
   }
