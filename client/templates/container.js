1078bff8a144283dfb31656d5dc309b9083f8d4e
Grapefruit <me@here.now>
2015-08-20 22:41:08 -0400

    active route highlighted for coursework top nav link

--- client/templates/container.js
+++ client/templates/container.js
@@ -37,6 +37,15 @@ Template.educationTopNavLink.helpers({
   }
 });
 
+Template.courseworkTopNavLink.helpers({
+  active: function() {
+    var bool = FlowRouter.getParam("name") == "coursework" ? "true" : "false";
+    console.log("education route is active? :: " + bool)
+    return "active-"+bool
+  }
+});
+
+
 Template.card.events({
   'click i.fa.fa-thumbs-o-up' : function(e,t) {
     e.preventDefault();
