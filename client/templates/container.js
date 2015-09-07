dc140066ef6167bf1b44733f8feb6e7260087259
Grapefruit <me@here.now>
2015-09-07 19:50:22 -0400

    renames card to cardContent for events and onRenderd block; buttons working

--- client/templates/container.js
+++ client/templates/container.js
@@ -46,7 +46,7 @@ Template.courseworkTopNavLink.helpers({
 });
 
 
-Template.card.events({
+Template.cardContent.events({
   'click i.fa.fa-thumbs-o-up' : function(e,t) {
     e.preventDefault();
     Meteor.call('upvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
@@ -61,7 +61,7 @@ Template.card.events({
   }
 });
 
-Template.card.onRendered( function() {
+Template.cardContent.onRendered( function() {
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
