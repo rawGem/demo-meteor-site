091f007501e74797c3525ec1974ce6b780cb5946
Grapefruit <me@here.now>
2015-09-13 15:58:21 -0400

    adds bl.ock icon logic via d3

--- client/templates/nav.js
+++ client/templates/nav.js
@@ -2,7 +2,9 @@ Template.side.onRendered( function() {
   $(".button-collapse").sideNav({
     closeOnClick : true
   });
+  d3MakeBlIcon();
 });
+
 
 Template.side.events({
   'click li#edu-side': function(e,t) {
@@ -40,3 +42,48 @@ Template.courseworkTopNavLink.helpers({
     return "active-"+bool
   }
 });
+<<<<<<< HEAD:client/templates/nav.js
+=======
+
+
+Template.cardContent.events({
+  'click i.fa.fa-thumbs-o-up' : function(e,t) {
+    e.preventDefault();
+    Meteor.call('upvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
+  },
+  'click': function(e,t) {
+  },
+  'click i.fa.fa-thumbs-o-down' : function(e,t) {
+    e.preventDefault();
+    Meteor.call('downvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
+  },
+  'click': function(e,t) {
+  }
+});
+
+Template.cardContent.onRendered( function() {
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  }); 
+} )
+
+
+//Template.main.helpers({
+//  sections: function() {
+//    return Sections.find();
+//  }
+//});
+
+
+function d3MakeBlIcon() {
+  var svg = d3.select("div.bl-icon").append("svg")
+               .attr("width", 20)
+               .attr("height", 20)
+
+  var blocks = svg.append("rect")
+                   .attr("width", 20)
+                   .attr("height", 20)
+                   .style("fill", "white")
+}
+>>>>>>> adds bl.ock icon logic via d3:client/templates/container.js
