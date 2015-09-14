9c07600390790c654929a8c294e9e44f4d0bb8ec
Grapefruit <me@here.now>
2015-09-14 01:21:55 -0400

    move templates' events and helpers to seperate files, one for card, one for navigation

--- client/templates/nav.js
+++ client/templates/nav.js
@@ -1,7 +1,3 @@
-/* container helpers */
-SBJCTX = new ReactiveVar();
-
-
 Template.side.onRendered( function() {
   $(".button-collapse").sideNav({
     closeOnClick : true
@@ -44,33 +40,3 @@ Template.courseworkTopNavLink.helpers({
     return "active-"+bool
   }
 });
-
-
-Template.cardContent.events({
-  'click i.fa.fa-thumbs-o-up' : function(e,t) {
-    e.preventDefault();
-    Meteor.call('upvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
-  },
-  'click': function(e,t) {
-  },
-  'click i.fa.fa-thumbs-o-down' : function(e,t) {
-    e.preventDefault();
-    Meteor.call('downvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
-  },
-  'click': function(e,t) {
-  }
-});
-
-Template.cardContent.onRendered( function() {
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  }); 
-} )
-
-
-//Template.main.helpers({
-//  sections: function() {
-//    return Sections.find();
-//  }
-//});
