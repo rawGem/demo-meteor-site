8216eee00f3a89209094339b8781d244f2a36ef2
Grapefruit <me@here.now>
2015-09-18 20:34:47 -0400

    deactivate card icons if user is not logged in

--- client/templates/card.js
+++ client/templates/card.js
@@ -1,17 +1,25 @@
 Template.cardContent.events({
   'click i.fa.fa-thumbs-o-up' : function(e,t) {
     e.preventDefault();
-    Meteor.call('upvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
+    if (Meteor.userId()) {
+        Meteor.call('upvoteWithHeadingField', 
+                [Template.parentData()._id, t.data.heading])
+    }
   },
   'click i.fa.fa-thumbs-o-down' : function(e,t) {
     e.preventDefault();
-    Meteor.call('downvoteWithHeadingField', [Template.parentData()._id, t.data.heading])
+    if (Meteor.userId()) {
+        Meteor.call('downvoteWithHeadingField', 
+                [Template.parentData()._id, t.data.heading])
+    }
   }
 });
 
 Template.cardContent.onRendered( function() {
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  }); 
-} )
+  if (Meteor.userId()) {
+    $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal();
+    }); 
+  }
+})
