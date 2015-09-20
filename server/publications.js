5f04099bbbac45e378739822bdf85608865614fd
Grapefruit <me@here.now>
2015-09-20 16:25:08 -0400

    add publication for all sections

--- server/publications.js
+++ server/publications.js
@@ -2,3 +2,8 @@
 Meteor.publish('sections', function( sectionName ) {
   return Sections.find( { subject : sectionName } );
 });
+
+Meteor.publish('allsections', function() {
+  // check for admin here? 
+  return Sections.find();
+})
