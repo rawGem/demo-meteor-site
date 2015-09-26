ab2c0a348db10b28faa4272518772ca565b93503
Grapefruit <me@here.now>
2015-09-25 23:24:43 -0400

    sub, pub, and template for messages

--- client/templates/admin_sections.js
+++ client/templates/admin_sections.js
@@ -2,6 +2,7 @@ Template.admin.onRendered( function() {
   var instance = this;
   this.autorun(function() {
     instance.subscribe('allsections');
+    instance.subscribe('allmessages');
   })
 });
 
@@ -20,7 +21,33 @@ Template.adminSections.helpers({
       }
       return Sections.find();
     }
+  /* 
+  /  admin_section wants to subscribe to 
+  /  messages by heading_id
+  //
+  /
+  /  want `heading_id` from `adminSections` helper
+  /  `sections`
+  /*
+  /*messages: function() {
+      if (!Meteor.user()) { //add admin check
+        return null;
+      }
+      return Messages.find();
+  }*/
 });
+
+
+Template.adminSection.helpers({
+  messages : 
+    function() {
+        data = Template.currentData();
+        //messages = Messages.find({heading_id : data.heading_id}).fetch()
+        messages = Messages.find({ heading_id : data.heading_id })
+        return messages
+    }
+})
+
 
 Template.admin.events({
   'click a': function(e,t) {
--- server/publications.js
+++ server/publications.js
@@ -3,6 +3,9 @@ Meteor.publish('sections', function( sectionName ) {
   return Sections.find( { subject : sectionName } );
 });
 
+
+
+
 Meteor.publish('allsections', function() {
   // check for admin here? 
   return Sections.find();
@@ -10,4 +13,20 @@ Meteor.publish('allsections', function() {
 
 Meteor.publish('allcontent', function() {
   return Content.find();
+});
+
+
+
+Meteor.publish('allmessages', function() {
+  // check for admin here? 
+  return Messages.find();
+})
+
+
+
+
+
+Meteor.publish('messages_by_heading', function(heading_id) {
+  // check for admin here? 
+  return Messages.find({heading_id : heading_id});
 })
