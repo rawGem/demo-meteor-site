a7fa3dc13e6e94954b6eb0b222b10a5869f68ed5
Grapefruit <me@here.now>
2015-10-06 16:04:21 -0400

    seed info for coursework

--- server/seed.js
+++ server/seed.js
@@ -9,18 +9,11 @@ Meteor.startup(function(){
         subject: "education",
         subsections : [
           {
-            heading     : "School 1",
+            heading     : "Georgia Institute of Technology",
             heading_id  : "school1",
-            text_region : "i lernt da codez",
+            text_region : "Major: Biomedical Engineering\n Minor: Organic Chemistry",
             upvotes     : 5,
             downvotes   : 1
-          },
-          {
-            heading     : "School 2",
-            heading_id  : "school2",
-            text_region : "lernt moar cdez",
-            upvotes     : 10,
-            downvotes   : 0
           }
         ]
       });
@@ -30,16 +23,23 @@ Meteor.startup(function(){
         subject     : "coursework",
         subsections : [
           {
-            heading     : "CS 101",
-            heading_id  : "cs101",
-            text_region : "i lernt da codez",
+            heading     : "cs188.1x, Artificial Intelligence",
+            heading_id  : "cs188x",
+            text_region : "BFS, DFS, A*, MDPs",
             upvotes     : 0,
             downvotes   : 0
           },
           {
-            heading     : "CS 102",
-            heading_id  : "cs102",
-            text_region : "lernt moar cdez",
+            heading     : "cs169.1x, SaaS",
+            heading_id  : "cs169",
+            text_region : "Agile, TDD, BDD, REST",
+            upvotes     : 0,
+            downvotes   : 0
+          },
+          {
+            heading     : "cs169.2x, SaaS",
+            heading_id  : "cs169",
+            text_region : "Legacy Code, Design Patterns, Version Control",
             upvotes     : 0,
             downvotes   : 0
           }
@@ -48,8 +48,8 @@ Meteor.startup(function(){
   }
   if (Meteor.users.find({username : "sjb"}).count() === 0) {
     var options = { 
-                    email : "me@here.now",
-                    username : "sjb",
+                    email : "admin@this.domain",
+                    username : "admin",
                     password : "apple8",
                     profile : { admin : true }
                   }
