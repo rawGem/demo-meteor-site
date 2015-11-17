d439d21650592b5f079d3c5474da1c536ea1583a
Grapefruit <me@here.now>
2015-11-17 12:26:50 -0500

    added lorem to section 1 in seed

--- server/genericseed.js
+++ server/genericseed.js
@@ -155,13 +155,13 @@ Meteor.startup(function(){
       },
       {
         heading_id   : "sect1",
-        topic        : "Major",
-        text         : "Biomedical Engineering"
+        topic        : "Subsection 1",
+        text         : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         heading_id   : "sect1",
-        topic        : "Minor",
-        text         : "Organic Chemistry"
+        topic        : "Subsection 2",
+        text         : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         heading_id   : "pkgs",
